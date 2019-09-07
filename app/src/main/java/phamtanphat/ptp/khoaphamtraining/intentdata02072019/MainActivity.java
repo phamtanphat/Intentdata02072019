07eb3be272b581de5126a284b2d0bb36ffbfcb47
phatdroid94 <phatdroid94@gmail.com>
2019-09-07 18:44:41 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentdata02072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentdata02072019/MainActivity.java
@@ -2,6 +2,7 @@ package phamtanphat.ptp.khoaphamtraining.intentdata02072019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,9 +20,11 @@ public class MainActivity extends AppCompatActivity {
         btnIntentString = findViewById(R.id.textviewString);
 
         //1 . 8 : lambda
-        btnIntentString.setOnClickListener(view ->
-                Toast.makeText(this, "Intent", Toast.LENGTH_SHORT).show()
-        );
+        btnIntentString.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+            intent.putExtra("chuoi","Hello main 2");
+            startActivity(intent);
+        });
 
     }
 }
