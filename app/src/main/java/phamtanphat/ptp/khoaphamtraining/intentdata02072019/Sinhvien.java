637dd86d38a652461deb1014df67c2079e392740
phatdroid94 <phatdroid94@gmail.com>
2019-09-10 18:17:40 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentdata02072019/Sinhvien.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/intentdata02072019/Sinhvien.java
@@ -1,6 +1,8 @@
 package phamtanphat.ptp.khoaphamtraining.intentdata02072019;
 
-public class Sinhvien {
+import java.io.Serializable;
+
+public class Sinhvien implements Serializable {
     String ten;
 
     public Sinhvien(String ten) {
